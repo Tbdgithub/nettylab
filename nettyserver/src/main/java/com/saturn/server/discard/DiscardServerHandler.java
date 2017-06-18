fd272cca8520b994752b5e66f22f7fd6777425fa
lyz <thatis2d@comeon.com>
2017-06-18 16:49:26 +0800

    discard

---
+++ nettyserver/src/main/java/com/saturn/server/discard/DiscardServerHandler.java
@@ -0,0 +1,24 @@
+package com.saturn.server.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by lyz on 2017/6/18.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ((ByteBuf) msg).release(); // (3)
+        System.out.println(" ((ByteBuf) msg).release()");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
