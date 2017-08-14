113c6cc6bb744cc3781dfdc509b90f8cae2ced28
ybname <yibo@feinno.com>
2017-08-14 09:42:37 +0800

    add func1

--- nettyserver/src/main/java/com/saturn/server/transfer/Port2Port.java
+++ nettyserver/src/main/java/com/saturn/server/transfer/Port2Port.java
@@ -4,4 +4,10 @@ package com.saturn.server.transfer;
  * Created by john.y on 2017-8-14.
  */
 public class Port2Port {
+
+    public void func1()
+    {
+        int a=10;
+        System.out.println(a);
+    }
 }
