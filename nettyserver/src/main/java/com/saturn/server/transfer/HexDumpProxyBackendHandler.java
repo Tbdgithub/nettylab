b6e0ea3475c926915af96b8716003b84a3b3afd9
ybname <yibo@feinno.com>
2017-08-14 10:38:28 +0800

    add simple port 2  port proxy

---
+++ nettyserver/src/main/java/com/saturn/server/transfer/HexDumpProxyBackendHandler.java
@@ -0,0 +1,45 @@
+package com.saturn.server.transfer;
+
+import io.netty.channel.*;
+
+/**
+ * Created by john.y on 2017-8-14.
+ */
+public class HexDumpProxyBackendHandler extends ChannelInboundHandlerAdapter {
+
+    private final Channel inboundChannel;
+
+    public HexDumpProxyBackendHandler(Channel inboundChannel) {
+        this.inboundChannel = inboundChannel;
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.read();
+    }
+
+    @Override
+    public void channelRead(final ChannelHandlerContext ctx, Object msg) {
+        inboundChannel.writeAndFlush(msg).addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) {
+                if (future.isSuccess()) {
+                    ctx.channel().read();
+                } else {
+                    future.channel().close();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) {
+        HexDumpProxyFrontendHandler.closeOnFlush(inboundChannel);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        HexDumpProxyFrontendHandler.closeOnFlush(ctx.channel());
+    }
+}
