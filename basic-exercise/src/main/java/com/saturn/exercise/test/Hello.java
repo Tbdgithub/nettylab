c11c47f7dc2e67702aea4a64bc084045c6d76fbf
ybname <yibo@feinno.com>
2017-09-11 11:59:20 +0800

    add exercise module

---
+++ basic-exercise/src/main/java/com/saturn/exercise/test/Hello.java
@@ -0,0 +1,7 @@
+package com.saturn.exercise.test;
+
+/**
+ * Created by john.y on 2017-9-11.
+ */
+public class Hello {
+}
---
+++ basic-exercise/src/test/java/com/saturn/exercise/test/HiTest.java
@@ -0,0 +1,14 @@
+package com.saturn.exercise.test;
+
+import org.junit.Test;
+
+/**
+ * Created by john.y on 2017-9-11.
+ */
+public class HiTest {
+
+    @Test
+    public void test1() {
+        System.out.println("hi");
+    }
+}
