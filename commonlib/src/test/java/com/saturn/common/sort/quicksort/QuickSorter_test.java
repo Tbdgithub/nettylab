525ca02477864a6912f28d4ad90544829a5cd30e
lyz <thatis2d@comeon.com>
2018-01-28 19:28:12 +0800

    quicksort exercise

--- commonlib/src/test/java/com/saturn/common/sort/quicksort/QuickSorter_test.java
+++ commonlib/src/test/java/com/saturn/common/sort/quicksort/QuickSorter_test.java
@@ -25,4 +25,22 @@ public class QuickSorter_test {
         }
 
     }
+
+    @Test
+    public void test2()
+    {
+        try {
+
+
+            Integer[] A = {2, 8, 7, 1, 3, 5, 6, 4};
+
+            System.out.println(new Gson().toJson(A));
+            QuickSortHelper.quickSort(A, 0, A.length - 1);
+            System.out.println(new Gson().toJson(A));
+        }catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+
+    }
 }
