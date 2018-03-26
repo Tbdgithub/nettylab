bffa566a38b7a49a3790891d115366e8789c0ba6
易波 <yibo@feinno.com>
2018-03-26 16:32:12 +0800

    sum two

--- commonlib/src/main/java/com/saturn/common/peak/PeakFinder.java
+++ commonlib/src/main/java/com/saturn/common/peak/PeakFinder.java
@@ -1,8 +1,20 @@
 package com.saturn.common.peak;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PeakFinder {
 
-    public int getPeak1(int[] a) {
+
+    public int getPeakLinear(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1])
+                return i;
+        }
+        return nums.length - 1;
+    }
+
+    public int getPeak0(int[] a) {
         for (int i = 0; i < a.length; i++) {
             if ((i - 1) > 0 && (a[i] >= a[i - 1])
                     && (a[i] >= a[i + 1])
@@ -35,7 +47,7 @@ public class PeakFinder {
             } else if (a[middle] == a[middle + 1]) {
 
                 //无法判断,左分，右分均可
-               // right = middle;
+                // right = middle;
                 left = middle + 1;
 
             } else {
@@ -45,11 +57,79 @@ public class PeakFinder {
 
             }
 
-            System.out.println("middle:"+middle+" ["+left+","+right+"]");
+            // System.out.println("middle:"+middle+" ["+left+","+right+"]");
         }
 
-         //left ==right 时
+        //left ==right 时
         return left;
+    }
+
+    public int findPeakElement(int[] nums) {
+        return search(nums, 0, nums.length - 1);
+    }
+
+    public int search(int[] nums, int l, int r) {
+        if (l == r)
+            return l;
+        int mid = (l + r) / 2;
+        if (nums[mid] > nums[mid + 1]) {
+
+            //right:mid 关键
+            return search(nums, l, mid);//
+        } else {
+            //<=相同处理
+            //l:mid+1 关键
+            return search(nums, mid + 1, r);
+        }
+    }
+
+    public int[] twoSumMap(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+            //关键 nums[i] 为key, i 为value
+            //
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            //化运算为查找key
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                //complement 不能等于i .即，相加的两数不相等
+                return new int[] { i, map.get(complement) };
+            }
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int count=0;
+
+        for (int i = 0; i < nums.length ; i++) {
+
+            for(int j=i+1;j<nums.length;j++)
+            {
+                count++;
+                //0: 1,2,3
+                //1:   2,3
+                //2:    ,3
+                //3:
+                //关键：j=i+1; i不等于j
+                // k <i 的已经比较过了
+                //所以j 从i+1开始,闭区间
+                if(nums[i]+nums[j]==target)
+                {
+                    return new int[] {i,j};
+                    //count++;
+                }
+            }
+        }
+
+       // System.out.println("count:"+count);
+
+        return nums;
+
     }
 
 
--- commonlib/src/test/java/com/saturn/common/PeakFinderTest.java
+++ commonlib/src/test/java/com/saturn/common/PeakFinderTest.java
@@ -1,5 +1,6 @@
 package com.saturn.common;
 
+import com.google.gson.Gson;
 import com.saturn.common.peak.PeakFinder;
 import org.junit.Test;
 
@@ -11,13 +12,62 @@ public class PeakFinderTest {
 
         //int[] a = {1, 2, 3, 3, 2, 1};
         //
-        int[] a = {1, 2, 3, 3, 4, 4, 5, 2, 1,1,7,7,1};
+        //int[] a = {1, 2, 3, 3, 4, 4, 5, 2, 1,1,7,7,1};
+        int [] a={1,2,3,1,4,1,5,1,6,1};
         PeakFinder finder = new PeakFinder();
-        int result = finder.getPeak1(a);
+//        int result = finder.getPeak1(a);
+//        System.out.println("result:" + result);
+
+       int  result = finder.getPeakBin(a);
         System.out.println("result:" + result);
 
-        result = finder.getPeakBin(a);
+    }
+
+    @Test
+    public void PeakFinderTest0() {
+
+        int [] a={1,2,2,3,3,1,4,1,5,1,6,1};
+        PeakFinder finder = new PeakFinder();
+//        int result = finder.getPeak1(a);
+//        System.out.println("result:" + result);
+
+        int  result = finder.getPeakLinear(a);
         System.out.println("result:" + result);
 
+    }
+
+    @Test
+    public void PeakFinderRecursiveTest() {
+
+        //int [] a={1,2,2,3,3,1,4,1,5,1,6,1};
+        int [] a={1, 2, 3, 1};
+        PeakFinder finder = new PeakFinder();
+//        int result = finder.getPeak1(a);
+//        System.out.println("result:" + result);
+
+        int  result = finder.findPeakElement(a);
+        System.out.println("result:" + result);
+
+    }
+
+    @Test
+    public void test4()
+    {
+        int [] a={2, 7, 11, 15};
+        PeakFinder finder=new PeakFinder();
+       int [] result= finder.twoSum(a,18);
+       System.out.println("result:"+new Gson().toJson(result));
+
+
+    }
+
+    @Test
+    public void test5()
+    {
+        int [] a={2, 7, 11, 15};
+        PeakFinder finder=new PeakFinder();
+        int [] result= finder.twoSumMap(a,26);
+
+        System.out.println("result:"+new Gson().toJson(result));
     }
 }
