174263f20ca7c95a6da7e46e922d81e2413c0707
易波 <yibo@feinno.com>
2018-03-26 16:42:52 +0800

    sum two map

--- commonlib/src/main/java/com/saturn/common/peak/PeakFinder.java
+++ commonlib/src/main/java/com/saturn/common/peak/PeakFinder.java
@@ -102,6 +102,23 @@ public class PeakFinder {
         throw new IllegalArgumentException("No two sum solution");
     }
 
+    public int[] twoSumOnePass(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+            //一次扫描
+            //先check
+            //再put
+            //put的key 是num的值,value 是index ;反转
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
     public int[] twoSum(int[] nums, int target) {
 
         int count=0;
--- commonlib/src/test/java/com/saturn/common/PeakFinderTest.java
+++ commonlib/src/test/java/com/saturn/common/PeakFinderTest.java
@@ -2,6 +2,7 @@ package com.saturn.common;
 
 import com.google.gson.Gson;
 import com.saturn.common.peak.PeakFinder;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PeakFinderTest {
@@ -13,12 +14,12 @@ public class PeakFinderTest {
         //int[] a = {1, 2, 3, 3, 2, 1};
         //
         //int[] a = {1, 2, 3, 3, 4, 4, 5, 2, 1,1,7,7,1};
-        int [] a={1,2,3,1,4,1,5,1,6,1};
+        int[] a = {1, 2, 3, 1, 4, 1, 5, 1, 6, 1};
         PeakFinder finder = new PeakFinder();
 //        int result = finder.getPeak1(a);
 //        System.out.println("result:" + result);
 
-       int  result = finder.getPeakBin(a);
+        int result = finder.getPeakBin(a);
         System.out.println("result:" + result);
 
     }
@@ -26,12 +27,12 @@ public class PeakFinderTest {
     @Test
     public void PeakFinderTest0() {
 
-        int [] a={1,2,2,3,3,1,4,1,5,1,6,1};
+        int[] a = {1, 2, 2, 3, 3, 1, 4, 1, 5, 1, 6, 1};
         PeakFinder finder = new PeakFinder();
 //        int result = finder.getPeak1(a);
 //        System.out.println("result:" + result);
 
-        int  result = finder.getPeakLinear(a);
+        int result = finder.getPeakLinear(a);
         System.out.println("result:" + result);
 
     }
@@ -40,34 +41,43 @@ public class PeakFinderTest {
     public void PeakFinderRecursiveTest() {
 
         //int [] a={1,2,2,3,3,1,4,1,5,1,6,1};
-        int [] a={1, 2, 3, 1};
+        int[] a = {1, 2, 3, 1};
         PeakFinder finder = new PeakFinder();
 //        int result = finder.getPeak1(a);
 //        System.out.println("result:" + result);
 
-        int  result = finder.findPeakElement(a);
+        int result = finder.findPeakElement(a);
         System.out.println("result:" + result);
 
     }
 
     @Test
-    public void test4()
-    {
-        int [] a={2, 7, 11, 15};
-        PeakFinder finder=new PeakFinder();
-       int [] result= finder.twoSum(a,18);
-       System.out.println("result:"+new Gson().toJson(result));
+    public void test4() {
+        int[] a = {2, 7, 11, 15};
+        PeakFinder finder = new PeakFinder();
+        int[] result = finder.twoSum(a, 18);
+        System.out.println("result:" + new Gson().toJson(result));
 
 
     }
 
     @Test
-    public void test5()
-    {
-        int [] a={2, 7, 11, 15};
-        PeakFinder finder=new PeakFinder();
-        int [] result= finder.twoSumMap(a,26);
+    public void test5() {
+        int[] a = {2, 7, 11, 15};
+        PeakFinder finder = new PeakFinder();
+        int[] result = finder.twoSumMap(a, 26);
 
-        System.out.println("result:"+new Gson().toJson(result));
+        System.out.println("result:" + new Gson().toJson(result));
+    }
+
+    @Test
+    public void test6() {
+        int[] a = {2, 7, 11, 15};
+        PeakFinder finder = new PeakFinder();
+        int[] result = finder.twoSumOnePass(a, 26);
+
+        Assert.assertTrue(result.length == 2 && result[0] == 2 && result[1] == 3);
+
+        System.out.println("result:" + new Gson().toJson(result));
     }
 }
