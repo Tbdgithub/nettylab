a40055cb51bef34a34420c7ff473c3c1bc36af18
易波 <yibo@feinno.com>
2018-03-28 15:59:27 +0800

    reversed integer

---
+++ commonlib/src/main/java/com/saturn/common/peak/ReverseInteger.java
@@ -0,0 +1,58 @@
+package com.saturn.common.peak;
+
+public class ReverseInteger {
+//
+//    Input: 123
+//    Output:  321
+//    Example 2:
+//
+//    Input: -123
+//    Output: -321
+//    Example 3:
+//
+//    Input: 120
+//    Output: 21
+
+    public static void main(String[] args) {
+
+        //  int result = reverse(1534236469);
+        int result = reverse(-2147483648);
+        System.out.println("result:" + result);
+    }
+
+    public static int reverse(int x) {
+
+        //1.10进制，循环取余数
+        //2. 结束条件,当前除值为0
+        //3.正负
+        //4. overflow check
+
+        //long current = Math.abs((long)x);
+        //-2147483648
+//        System.out.println("is minuse:" +( Integer.MIN_VALUE==-Integer.MIN_VALUE));
+//        System.out.println(x<0);
+//        System.out.println((-x)<0);
+        long current = Math.abs(x);
+        long sum = 0;
+
+        do {
+            long m = current / 10;
+            long n = current % 10;
+            //System.out.println(n);
+            sum = (sum * 10 + n);
+            current = m;
+        }
+        while (current > 0);
+
+        if (x < 0) {
+            sum = sum * -1;
+        }
+
+        if (sum > Integer.MAX_VALUE || sum < Integer.MIN_VALUE) {
+            sum = 0;
+        }
+
+        return (int) sum;
+    }
+
+}
