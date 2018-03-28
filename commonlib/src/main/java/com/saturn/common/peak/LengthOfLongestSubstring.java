0c98f0a99b4530889339978c4d05709fe7463e1f
易波 <yibo@feinno.com>
2018-03-28 18:19:16 +0800

    lengthOfLongestSubstring

--- commonlib/src/main/java/com/saturn/common/peak/LengthOfLongestSubstring.java
+++ commonlib/src/main/java/com/saturn/common/peak/LengthOfLongestSubstring.java
@@ -1,5 +1,7 @@
 package com.saturn.common.peak;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -18,35 +20,43 @@ public class LengthOfLongestSubstring {
         System.out.println("result:" + result);
     }
 
+    public static int lengthOfLongestSubstring_slidewindow_opt(String s) {
+
+        //1.windows 放不同的
+        //2.向右移动
+        //
+
+        throw new NotImplementedException();
+    }
+
     public static int lengthOfLongestSubstring_slidewindow(String s) {
 
         //1.windows 放不同的
         //2.向右移动
 
-        Set<Character> set=new HashSet<>();
+        Set<Character> set = new HashSet<>();
 
-        int i=0;
-        int j=0;
-        int N=s.length();
-        int result=0;
+        int i = 0;
+        int j = 0;
+        int N = s.length();
+        int result = 0;
 
-        while (i<N && j<N)
-        {
-            if(!set.contains(s.charAt(j)))
-            {
+        while (i < N && j < N) {
+            if (!set.contains(s.charAt(j))) {
                 //j 为右端
                 set.add(s.charAt(j));
                 ++j;//右端右移一步;
-                result=Math.max(result,j-i);
-            }
-            else
-            {
+                result = Math.max(result, j - i);//最大的
+            } else {
                 //
-                set.remove(s.charAt(i));
+                set.remove(s.charAt(i));//因为i,与j 值重复了
                 ++i;// 左端右移一步
             }
         }
 
+        //todo 可输出子串;记录i,j
+
+        // System.out.println(s.substring(i,j));
         return result;
     }
 
