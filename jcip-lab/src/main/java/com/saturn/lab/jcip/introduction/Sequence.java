989075f28adbb3bf2a33f33e46589c1652228753
john <netfun2@hotmail.com>
2018-05-12 20:06:42 +0800

    settings

---
+++ jcip-lab/src/main/java/com/saturn/lab/jcip/introduction/Sequence.java
@@ -0,0 +1,13 @@
+package com.saturn.lab.jcip.introduction;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class Sequence {
+    @GuardedBy("this") private int nextValue;
+
+    public synchronized int getNext() {
+        return nextValue++;
+    }
+}
---
+++ jcip-lab/src/main/java/com/saturn/lab/jcip/introduction/UnsafeSequence.java
@@ -0,0 +1,16 @@
+package com.saturn.lab.jcip.introduction;
+
+import net.jcip.annotations.NotThreadSafe;
+
+@NotThreadSafe
+public class UnsafeSequence {
+
+    private int value;
+
+    /**
+     * Returns a unique value.
+     */
+    public int getNext() {
+        return value++;
+    }
+}
