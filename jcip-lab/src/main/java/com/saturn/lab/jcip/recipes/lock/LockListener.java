6b72d7f00dc095ea5cbbd1eb9f6a0ec05d68c8ae
易波 <yibo@feinno.com>
2018-06-21 22:13:46 +0800

    官方的zookeeper lock

---
+++ jcip-lab/src/main/java/com/saturn/lab/jcip/recipes/lock/LockListener.java
@@ -0,0 +1,15 @@
+package com.saturn.lab.jcip.recipes.lock;
+
+public interface LockListener {
+    /**
+     * call back called when the lock
+     * is acquired
+     */
+    public void lockAcquired();
+
+    /**
+     * call back called when the lock is
+     * released.
+     */
+    public void lockReleased();
+}
