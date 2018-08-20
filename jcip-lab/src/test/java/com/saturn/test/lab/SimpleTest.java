a4dac6a897a4abe7d5d1abf4bc1e14d2cf305223
john <yibo@rongcloud.cn>
2018-08-20 10:58:27 +0800

    computeabsent

--- jcip-lab/src/test/java/com/saturn/test/lab/SimpleTest.java
+++ jcip-lab/src/test/java/com/saturn/test/lab/SimpleTest.java
@@ -3,6 +3,10 @@ package com.saturn.test.lab;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class SimpleTest {
 
     @Test
@@ -14,7 +18,28 @@ public class SimpleTest {
 
         System.out.println(a1 + " :" + a1.hashCode());
         System.out.println(a2 + " :" + a2.hashCode());
-        Assert.assertTrue(a1.hashCode()==a2.hashCode());
+        Assert.assertTrue(a1.hashCode() == a2.hashCode());
+
+    }
+
+    @Test
+    public void ComuteIfAbsentTest() {
+
+        HashMap<String, List<String>> dic = new HashMap<>();
+        List<String> oldList = new ArrayList<>();
+        System.out.println(oldList.hashCode());
+        // dic.putIfAbsent("a", oldList);
+
+        List<String> list = dic.computeIfAbsent("a", a ->
+
+                {
+                    //return new ArrayList<>();
+                    return oldList;
+                }
+        );
+
+        System.out.println(list.hashCode());
+        System.out.println(list);
 
     }
 
